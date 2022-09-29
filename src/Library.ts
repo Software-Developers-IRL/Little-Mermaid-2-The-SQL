fc8f80586a90706228375500dec82d589958d6ff
lastlink <theefunk@gmail.com>
2022-09-28 23:05:31 -0400

    expand cli and mermaid parsing

--- src/Library.ts
+++ src/Library.ts
@@ -11,12 +11,31 @@ export const GenerateSqlFromMermaid = async function (
 ): Promise<string> {
   if (mermaid) {
     try {
+      let config = {
+        theme: "forest",
+        sequence: {
+          showSequenceNumbers: true,
+        },
+      };
+      // gra mermaid charts
+      // from mermaid cli markdown parsing
+      // https://github.com/mermaid-js/mermaid-cli/blob/master/src/index.js
+      const mermaidChartsInMarkdown =
+        /^\s*```(?:mermaid)(\r?\n([\s\S]*?))```\s*$/;
+      const mermaidChartsInMarkdownRegexGlobal = new RegExp(
+        mermaidChartsInMarkdown,
+        "gm"
+      );
+      const mermaidChartsInMarkdownRegex = new RegExp(mermaidChartsInMarkdown);
+      var m = mermaid;
       //   var test = mermaid.parse(fileContents);
       var t = mermaid.mermaidAPI.getSiteConfig();
-      console.log(JSON.stringify(t));
+      mermaid.initThrowsErrors();
+      //   console.log(JSON.stringify(t));
       var defaultConfig = mermaid.mermaidAPI.defaultConfig;
-      console.log(JSON.stringify(defaultConfig));
-      mermaid.mermaidAPI.initialize(defaultConfig);
+      //   console.log(JSON.stringify(defaultConfig));
+      mermaid.mermaidAPI.initialize(t);
+      mermaid.mermaidAPI.render;
       var test = mermaid.mermaidAPI.parse(fileContents);
       console.log(JSON.stringify(test));
       // var config = mermaid.getSiteConfig();
